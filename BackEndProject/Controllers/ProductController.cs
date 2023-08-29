31f7395aa7eb00059e8068756ba7e69001196f4c
Douglas <douglas.rtoledo2003@gmail.com>
2023-08-29 10:25:51 -0300

    feat: added remove and view function

--- BackEndProject/Controllers/ProductController.cs
+++ BackEndProject/Controllers/ProductController.cs
@@ -17,14 +17,10 @@ namespace BackEnd.Project.Web.Controllers
         public IActionResult List()
         {
             ViewData["Title"] = "All Products";
-            ViewData["Date"] = DateTime.Now;
 
             var Products = _unitOfWork.Product.GetAll();
             return View(Products);
-            //var productListViewModel = new ProductListViewModel();
-            //productListViewModel.Products = _unitOfWork.GetAll();
-            //return View(productListViewModel);
-
+            
         }
 
         public IActionResult RemoveProduct(Product product)
@@ -32,30 +28,39 @@ namespace BackEnd.Project.Web.Controllers
 
             var productToBeDeleted = _unitOfWork.Product.FindById(product.Id);
 
-            //var productToBeDeleted = _productRepository.Product.Get(u => u.Id == id);
-            //var productListViewModel =
-            //     _productRepository.ProductListViewModel.SingleOrDefault(
-            //        s => s.Product.Id == product.Id && s.ProductListViewModel == ProductListViewModel);
-            //var quantidadeLocal = 0; 
 
-            if (productToBeDeleted != null)
-            {
-                if (productToBeDeleted.Products.Count > 1)
-                {
-                    productToBeDeleted.Quantity--;
-                    //quantidadeLocal = productListViewModel.Quantidade;
-                }
-                else
-                {
-                    //_productRepository.ProductListViewModel.Remove(productListViewModel);
-                    _unitOfWork.Product.Remove(productToBeDeleted);
-                }
-            }
+            //if (productToBeDeleted != null)
+            //{
+            //    if (productToBeDeleted.Products.Count > 1)
+            //    {
+            //        productToBeDeleted.Quantity--;                 
+            //    }
+            //    else
+            //    {
+            //        _unitOfWork.Product.Remove(productToBeDeleted);
+            //    }
+            //}
+            _unitOfWork.Product.Remove(productToBeDeleted);
             _unitOfWork.Save();
             
             return View();
         }
 
+        public IActionResult CreateProducts(Product product)
+        {
+            Product productToBeCreated = new Product()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Description = product.Description,
+                Quantity = product.Quantity,
+                InStock = product.InStock,
+            };
+            _unitOfWork.Save();
+            return View(productToBeCreated);
+
+        }
 
     }
 }
